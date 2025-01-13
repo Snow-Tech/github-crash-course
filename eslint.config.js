67ab8aca7947437501dfec435a0981271d59d073
Brian Vieira <>
2025-01-13 18:41:48 +0000

    modified lint json

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,9 @@
 import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
 import globals from "globals";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
 ];
