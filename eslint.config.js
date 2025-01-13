bcad80d89c2a05b96d639e66ae22121002b8fa6f
Brian Vieira <>
2025-01-13 18:27:16 +0000

    modified lint json

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,20 @@
-import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-
+import globals from "globals";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off", // Desabilita a regra que exige a importação do React
+    },
+  },
 ];
