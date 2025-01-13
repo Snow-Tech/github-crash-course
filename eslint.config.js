950069af3766553303e8360a256ffc7f1b4a6941
Brian Vieira <>
2025-01-13 18:22:22 +0000

    fix workflow

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,10 @@
 import globals from "globals";
-import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
   {languageOptions: { globals: globals.browser }},
-  ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
